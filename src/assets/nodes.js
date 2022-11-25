7dc1ef53136c232caa4e6105821dcbe965466d10
Angel Duarte <adavid.tinocod@gmail.com>
2022-11-24 21:56:11 -0600

    glosario hecho

--- src/assets/nodes.js
+++ src/assets/nodes.js
@@ -1,7 +1,12 @@
 const $ = (id) => document.getElementById(id);
 
 // => Homepage
+const homeWelcome  = $('home-welcome');
 const homeHero = $('home-hero');
 const homeSections  = $('home-sections');
+const homeMembers  = $('home-members');
+
+const glosarioSection  = $('glosario');
 
 const networkSection  = $('networking');
+const pdfNode = $('pdf');
